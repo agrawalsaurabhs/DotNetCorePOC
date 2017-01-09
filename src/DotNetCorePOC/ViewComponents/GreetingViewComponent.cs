1ab064ab6976e34ce9e2cf4f40b4a56e0aa68495
saurabh agrawal <hisaurabhagrawal@gmail.com>
2017-01-10 00:12:17 +0530

    Added greeting viewcomponent

---
+++ src/DotNetCorePOC/ViewComponents/GreetingViewComponent.cs
@@ -0,0 +1,24 @@
+﻿using DotNetCorePOC.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DotNetCorePOC.ViewComponents
+{
+    public class GreetingViewComponent : ViewComponent
+    {
+        private readonly IGreetingService _greetingservice;
+
+        public GreetingViewComponent(IGreetingService greetingservice )
+        {
+            _greetingservice = greetingservice;
+        }
+        public IViewComponentResult Invoke()
+        {
+            var model = _greetingservice.Message;
+            return View("Default", model);
+        }
+    }
+}
